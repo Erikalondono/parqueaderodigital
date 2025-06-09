aa9356c5436c757e050143f2268e7e9385f16089
erika rincon <erikalondo506@gmail.com>
2025-06-08 20:26:12 -0500

    Primer commit del proyecto NetBeans

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+package com.milibreria.vista;
+
+import com.milibreria.controlador.UsuarioDAO;
+import com.milibreria.modelo.Usuario;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        UsuarioDAO dao = new UsuarioDAO();
+        List<Usuario> usuarios = dao.listar();
+
+        for (Usuario u : usuarios) {
+            System.out.println(u.getId() + " " + u.getNombre() + " " + u.getCorreo());
+        }
+    }
+}
---
+++ src/main/java/UsuarioDAO.java
@@ -0,0 +1,29 @@
+package com.milibreria.controlador;
+
+import com.milibreria.modelo.Usuario;
+import com.milibreria.util.Conexion;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioDAO {
+
+
+    public List<Usuario> listar() {
+        List<Usuario> lista = new ArrayList<>();
+        String sql = "SELECT * FROM usuarios";
+        try (Connection con = Conexion.conectar();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+            while (rs.next()) {
+                lista.add(new Usuario(rs.getInt("id"),
+                                      rs.getString("nombre"),
+                                      rs.getString("correo")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+}
